6ff0607734adab4217f5248e8be808531cc5a1e5
beesou777 <sbeesou@gmail.com>
2024-09-18 12:43:25 +0545

    update : skeletan design updated

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Header, Footer, ExploreMenu, ListComponent } from './views/views';  
+import { Header, Footer, ExploreMenu, ListComponent,HeroSection,Flavour,Contact } from './views/views';  
 
 (async function () {
     const app = document.getElementById('app');
@@ -10,13 +10,19 @@ import { Header, Footer, ExploreMenu, ListComponent } from './views/views';
 
     try {
         const headerHTML = await Header();
+        const heroSection = await HeroSection();
+        const flavourHTML = await Flavour();
         const exploreMenuHTML = await ExploreMenu();
         const listComponentHTML = await ListComponent();
+        const contactHTML = await Contact();
         const footerHTML = await Footer();
         
         app.innerHTML += headerHTML;
+        app.innerHTML += heroSection;
+        app.innerHTML += flavourHTML;
         app.innerHTML += exploreMenuHTML;
         app.innerHTML += listComponentHTML;
+        app.innerHTML += contactHTML;
         app.innerHTML += footerHTML;
 
         const event = new CustomEvent('contentLoaded');
--- views/views.js
+++ views/views.js
@@ -21,3 +21,22 @@ export async function ListComponent() {
     const data = await response.text();
     return data;
 }
+
+export async function HeroSection() {
+    const response = await fetch('/components/HeroSection.html');
+    const data = await response.text();
+    return data;
+}
+
+export async function Flavour() {
+    const response = await fetch('/components/Flavour.html');
+    const data = await response.text();
+    return data;
+}
+
+
+export async function Contact() {
+    const response = await fetch('/components/Contact.html');
+    const data = await response.text();
+    return data;
+}
